7cf7760aab4374fa08f7e364235986368719cb91
VIDUSHISAINI29 <vidushisaini29@gmail.com>
2024-11-18 23:37:55 +0530

    1 more function added

--- FUNCTION.C
+++ FUNCTION.C
@@ -1,14 +1,20 @@
 #include<stdio.h>
 #include<conio.h>
 int getFact(int x);
+int getPower(int y, int z);
 void main()
 	{
-		int value, a ;
+		int factValue, factInput, powerInputA, powerInputB, powerValue  ;
 		clrscr();
 		printf("Enter a number of which you want factorial = ");
-		scanf("%d", &a);
-		value = getFact(a);
-		printf("Factorial of your number %d is %d", a, value);
+		scanf("%d", &factInput);
+		factValue = getFact(factInput);
+		printf("Factorial of your number %d is %d\n", factInput, factValue);
+		printf("\n");
+		printf("Enter two numbers a and b so that to calculate a raised to the power b \n");
+		scanf(" %d %d", &powerInputA, &powerInputB);
+		powerValue = getPower(powerInputA, powerInputB);
+		printf("Output of power calculation is %d", powerValue);
 		getch();
 	}
 int getFact(int x)
@@ -27,4 +33,15 @@ int getFact(int x)
 			}
 			return (factorial);
 		}
+
+	}
+
+int getPower(int y, int z)
+	{
+		int i, powered= 1;
+		for(i = 0; i < z; i++)
+		{
+			powered = powered * y;
+		}
+		return (powered);
 	}
